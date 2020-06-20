f5c2f04f14de1ea6cf652885b211604c7015a0e6
tatsuya828828 <kkjy011894@gmail.com>
2020-06-20 23:01:20 +0000

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/top'
-  get 'play/index'
-  get 'play/show'
+  root 'home#top'
+  resources :plays, only: [:show, :index, :create, :update] do
+  	collection do
+  		get '/:id/quests', to: 'plays#quests', as: 'quests'
+  	end
+  end
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
