2970c754e27b146e90c8a69ae67cd91ff99133be
tatsuya828828 <kkjy011894@gmail.com>
2020-06-20 11:05:53 +0000

    rateカラムの削除

--- db/migrate/20200620013150_create_questions.rb
+++ db/migrate/20200620013150_create_questions.rb
@@ -3,7 +3,6 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
     create_table :questions do |t|
       t.string :quest
       t.text :answer
-      t.integer :rate,				default: 0;
       t.integer :undisplay_switch,	default: 0;
 
       t.timestamps
