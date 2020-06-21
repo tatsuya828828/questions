f09dfcc5c0fea3d07b228652ed5474e2c0e8ef56
tatsuya828828 <kkjy011894@gmail.com>
2020-06-21 05:04:46 +0000

    enumの名称の変更

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,5 +1,5 @@
 class History < ApplicationRecord
 	belongs_to :play
 
-	enum collect_status: {before_collect: 0, uncollect: 1, collect: 2}
+	enum collect_status: {before_collect: 0, uncollect: 1, collected: 2}
 end
