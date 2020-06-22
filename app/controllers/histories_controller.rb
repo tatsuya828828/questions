b4a00a14de962c2eb0de29f0b982db7706ef3ff9
tatsuya828828 <kkjy011894@gmail.com>
2020-06-22 01:13:25 +0000

    正解/不正解の実装

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,2 +1,12 @@
 class HistoriesController < ApplicationController
+
+	def update
+	    history = History.find(params[:id])
+	    history.update(collect_status: params[:collect_status])
+	  	play = Play.find(params[:play_id])
+	  	collects = play.histories.where(collect_status: "collected")
+	  	score = collects.count*2
+	  	play.update(score: score)
+	  	redirect_back(fallback_location: root_path)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'home#top'
-  resources :plays, only: [:show, :index, :create, :update] do
+  resources :plays, only: [:show, :index, :create] do
   	collection do
   		get '/:id/quests', to: 'plays#quests', as: 'quests'
   		get '/:id/detail/:history_id', to: 'plays#detail', as: 'detail'
   	end
+  	resources :histories, only: [:update]
   end
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
